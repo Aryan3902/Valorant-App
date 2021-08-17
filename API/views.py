2400bec8acc7f0f0f888162ea8a9a8768d278bdc
OneRudeZombie <79202857+OneRudeZombie@users.noreply.github.com>
2021-08-17 11:58:34 +0530

    Version 2.1.0

--- API/views.py
+++ API/views.py
@@ -5,37 +5,42 @@ from django.shortcuts import render
 
 from .auth import run
 # Create your views here.
-headers = run('baburaooo7', 'mayank1234')
+headers = run('OnerudeZombie', 'Aryan1122?')
 content = requests.get(
     'https://shared.ap.a.pvp.net/content-service/v2/content', headers=headers).json()
 agent_pic = requests.get(
     'https://valorant-api.com/v1/agents', headers=headers).json()['data']
 puuid = requests.get(
-    'https://api.henrikdev.xyz/valorant/v1/account/{}/{}'.format("baburao", "1942"), headers=headers).json()['data']['puuid']
+    'https://api.henrikdev.xyz/valorant/v1/account/{}/{}'.format("OnerudeZombie", "NOOB"), headers=headers).json()['data']['puuid']
 all = requests.get(
     'https://valorant-api.com/v1/agents?isPlayableCharacter=true').json()['data']
 match = requests.get(
     'https://pd.ap.a.pvp.net/match-history/v1/history/{}'.format(puuid), headers=headers).json()['History']
+loadout = requests.get(
+    'https://pd.ap.a.pvp.net/personalization/v2/players/{}/playerloadout'.format(puuid), headers=headers).json()
 
 
 def home(request):
-
-    # headers = {
-    #     'Authorization': 'Bearer eyJraWQiOiJzMSIsInR5cCI6ImF0K2p3dCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyNjA1N2Q3ZS1iNTI3LTUzNTAtYTFhYy1lNmM1YmJlOTI4OGYiLCJzY3AiOlsib3BlbmlkIl0sImNsbSI6WyJvcGVuaWQiXSwiZGF0Ijp7ImxpZCI6IkhlRDBNa3FTOW1sbUlSc0lEOG5GclEiLCJjIjoiZWMxIn0sImlzcyI6Imh0dHBzOlwvXC9hdXRoLnJpb3RnYW1lcy5jb20iLCJleHAiOjE2MjcwNDkyNDIsImlhdCI6MTYyNzA0NTY0MiwianRpIjoiUk1PU2xkaXJZVjgiLCJjaWQiOiJwbGF5LXZhbG9yYW50LXdlYi1wcm9kIn0.T9fHWivaCdkWzrEAHDSpFsjmaSy5UgcJHeXBMPAoLlZWMLDKSgHcISJP1kC8qQ5FI7zj9RJg9BNtrCjR1NLdKi-m7zpTgiuBoQ4d_9LFhZWyjJYZnWm8pGtUG4Sunlj__PNfZwqfaQT_zTx52hmQNa8m0hzwewgnV2jJldtt4Mg',
-    #     'X-Riot-Entitlements-JWT': 'eyJraWQiOiJrMSIsImFsZyI6IlJTMjU2In0.eyJlbnRpdGxlbWVudHMiOltdLCJhdF9oYXNoIjoibi1yVm5WNEVJd3F6dGJHeVh6YS16ZyIsInN1YiI6IjI2MDU3ZDdlLWI1MjctNTM1MC1hMWFjLWU2YzViYmU5Mjg4ZiIsImlzcyI6Imh0dHBzOlwvXC9lbnRpdGxlbWVudHMuYXV0aC5yaW90Z2FtZXMuY29tIiwiaWF0IjoxNjI3MDQ1NjQzLCJqdGkiOiJSTU9TbGRpcllWOCJ9.mQCiCMTjX4b41Nm-qnqM7lIiJI0NZRkCorJStDGISZafB3760xtAJFfUEKRCHL_udajKBC5yRsYygmfj7HeKKLSaUvNDeDv4FM7F2oHL_bdCMEQ04DDiwL-T4PMIXtQIWWYuj-Y9BGUaTv5T679nJzv-NIwNt4ROPczQtzYQC4vQ3RqDkLGWUcu2OiNazHoo374NjyzXzjYJTwKVBkLw-tc4v7yNLgubEn0oREgnG7YfaGgno1O9EfUSLEnEmLmrzlBk4cuJEBk5Sxu0Da9-TGrCikOxECsZBiFspz_6cWh6DG5z8SEVNEGOoGXqqs3wJ2pm_bvISqkuSLP0R2dg_Q',
-    # }
-
+    titles = requests.get(
+        'https://valorant-api.com/v1/playertitles', headers=headers).json()['data']
+    cards = requests.get(
+        'https://valorant-api.com/v1/playercards', headers=headers).json()['data']
+    PlayerCard = loadout['Identity']['PlayerCardID']
+    PlayerTitle = loadout['Identity']['PlayerTitleID']
+    Title = []
+    Card = []
+    for card in cards:
+        if card['uuid'] == PlayerCard:
+            Card.append(card)
+    for title in titles:
+        if title['uuid'] == PlayerTitle:
+            Title.append(title)
     username = requests.get(
         'https://api.henrikdev.xyz/valorant/v1/account/{}/{}'.format("baburao", "1942"), headers=headers).json()['data']
     progress = requests.get(
         'https://pd.ap.a.pvp.net/account-xp/v1/players/{}'.format(puuid), headers=headers).json()['Progress']
 
-    # for map_letter in range(11, 20):
-    #     if match_info['matchInfo']['mapId'][map_letter] == '/':
-    #         break
-    #     map.append(match_info['matchInfo']['mapId'][map_letter])
-
-    return render(request, 'API/home.html', {'level': progress['Level'], 'xp': progress['XP'], 'username': username['name'], 'tag': username['tag']})
+    return render(request, 'API/home.html', {'level': progress['Level'], 'xp': progress['XP'], 'username': username['name'], 'tag': username['tag'], 'card': Card, 'title': Title})
 
 
 def Match(request):
